ea1276933e77e07b6239891a7a255b0e3269e9a2
Felipe Peña <felipe@penya.cl>
2018-11-04 21:01:53 -0800

    Add integratioin test

---
+++ tests/Feature/CleanerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Cleaner;
+use App\CleanHistory;
+use App\Employee;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CleanerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_cleaner_cannot_generate_cleaners_if_there_are_no_employees()
+    {
+        Cleaner::chooseCleaners();
+
+        $history = CleanHistory::all();
+
+        $this->assertTrue($history->count() == 0);
+    }
+
+    /** @test */
+    public function a_cleaner_can_generate_cleaners()
+    {
+        create(Employee::class, [], 2);
+
+        Cleaner::chooseCleaners();
+
+        $history = CleanHistory::all();
+
+        $this->assertTrue($history->count() > 0);
+    }
+}
